0ec4f8dab8e9047715c67b798b5540f780c9a7a9
Max Netterberg <max.netterberg@mevisio.com>
2024-04-17 16:00:35 +0200

    added markdown fixes

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,12 +1,26 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
 
 -- Remove unused import on save
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.ts*",
   command = ":TSToolsRemoveUnusedImports sync",
 })
+
+-- Fix conceallevel for markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("markdown_conceal"),
+  pattern = { "markdown" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
+
+-- Activate virtual environment for python
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "python" },
   callback = function()
