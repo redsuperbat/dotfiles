b2528142374315fd840d5f4710554fffedc03831
Max Netterberg <max.netterberg@mevisio.com>
2024-09-08 17:44:34 +0200

    use null ls instead of conform

--- nvim/lua/max/plugins/lsp/nvim-none-ls.lua
+++ nvim/lua/max/plugins/lsp/nvim-none-ls.lua
@@ -1,17 +1,64 @@
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    async = false,
+    filter = function(client)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  })
+end
+
 return {
-  event = { "BufReadPost", "BufWritePost", "BufNewFile" },
   "nvimtools/none-ls.nvim",
+  event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        lsp_formatting(vim.api.nvim_get_current_buf())
+      end,
+      mode = { "n" },
+      desc = "Format buffer",
+    },
+  },
   config = function()
     local nls = require("null-ls")
-    nls.setup()
-    nls.builtins.formatting.biome.with({
-      args = {
-        "check",
-        "--apply-unsafe",
-        "--skip-errors",
-        "--formatter-enabled=true",
-        "--organize-imports-enabled=true",
-        "$FILENAME",
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      callback = function()
+        lsp_formatting(vim.api.nvim_get_current_buf())
+      end,
+    })
+
+    nls.setup({
+      sources = {
+        nls.builtins.formatting.shfmt,
+        nls.builtins.formatting.stylua,
+        nls.builtins.formatting.biome.with({
+          args = {
+            "format",
+            "--write",
+            "--indent-style",
+            "space",
+            "--stdin-file-path",
+            "$FILENAME",
+          },
+        }),
+
+        nls.builtins.diagnostics.fish,
+        nls.builtins.formatting.fish_indent,
+
+        nls.builtins.formatting.prettier.with({
+          -- Exclude js/ts json because biome handles that
+          disabled_filetypes = {
+            "javascript",
+            "typescript",
+            "javascriptreact",
+            "typescriptreact",
+            "json",
+            "jsonc",
+          },
+        }),
       },
     })
   end,
