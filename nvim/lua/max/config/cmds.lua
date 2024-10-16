ce54d09a2b1caa86b79f910c2579f07dc170c876
Max Netterberg <max.netterberg@mevisio.com>
2024-10-16 17:13:28 +0200

    proper search and replae

--- nvim/lua/max/config/cmds.lua
+++ nvim/lua/max/config/cmds.lua
@@ -1,6 +1,20 @@
+function os.capture(cmd, raw)
+  local f = assert(io.popen(cmd, "r"))
+  local s = assert(f:read("*a"))
+  f:close()
+  if raw then
+    return s
+  end
+  s = string.gsub(s, "^%s+", "")
+  s = string.gsub(s, "%s+$", "")
+  s = string.gsub(s, "[\n\r]+", " ")
+  return s
+end
+
 vim.api.nvim_create_user_command("SearchReplace", function(opts)
   local args = opts.fargs
 
+  local result = os.capture(string.format("rg -l '%s'", args[1]))
   local command = string.format(
     "rg -l '%s' | xargs -I§ -- sh -c 'rg \"%s\" -r \"%s\" --passthru § | sponge §'",
     args[1],
@@ -9,7 +23,8 @@ vim.api.nvim_create_user_command("SearchReplace", function(opts)
     args[3] or "."
   )
   os.execute(command)
+  vim.print(result)
 end, {
   desc = "Search and replace with path",
-  nargs = "?",
+  nargs = "*",
 })
