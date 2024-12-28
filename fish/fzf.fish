b5ef7f9aaa2e8f743acbadf1e99ddb092c5f57dd
Max Netterberg <max.netterberg@gmail.com>
2024-12-28 13:20:40 +0100

    set default command in tmux conf for a specific path

--- fish/fzf.fish
+++ fish/fzf.fish
@@ -1,2 +1,2 @@
 export FZF_DEFAULT_OPTS="--ansi --preview-window 'right:60%' --preview 'bat --color=always --style=numbers --line-range :300 {}'"
-export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix'
+export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --exclude .git'
