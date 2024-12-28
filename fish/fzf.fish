2ae13633268d4f6a5d367b589d94c9b2c9062d8e
Max Netterberg <max.netterberg@gmail.com>
2024-12-28 13:07:45 +0100

    add default command

--- fish/fzf.fish
+++ fish/fzf.fish
@@ -1,1 +1,2 @@
-export FZF_DEFAULT_OPTS="--ansi --preview-window 'right:60%' --preview 'bat --color=always --style=header,grid --line-range :300 {}'"
+export FZF_DEFAULT_OPTS="--ansi --preview-window 'right:60%' --preview 'bat --color=always --style=numbers --line-range :300 {}'"
+export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix'
