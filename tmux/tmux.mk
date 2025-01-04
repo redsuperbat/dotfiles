632b1ac7265c58141f0d4a458df51a70467aa681
Max Netterberg <max.netterberg@gmail.com>
2025-01-04 15:51:10 +0100

    add tms

--- tmux/tmux.mk
+++ tmux/tmux.mk
@@ -1,5 +1,6 @@
 TMUX = $(BIN_DIR)/tmux
 TMUX_CONFIG_DIR = $(CONFIG_DIR)/tmux
+TMS_CONFIG_DIR = $(CONFIG_DIR)/tms
 TPM_DIR = $(TMUX_CONFIG_DIR)/plugins/tpm
 
 .PHONY: tmux
@@ -8,7 +9,9 @@ tmux: $(TMUX)
 
 $(TMUX): $(BREW) $(CONFIG_DIR) $(FISH)
 	( $(BREW) install tmux )
+	( $(BREW) install tmux-sessionizer)
 	ln -his "$(PWD)/tmux" $(TMUX_CONFIG_DIR)
+	ln -his "$(PWD)/tmux/tms" $(TMUX_CONFIG_DIR)
 	[ -d $(TPM_DIR) ] || git clone https://github.com/tmux-plugins/tpm $(TPM_DIR)
 	# Ignore all errors
 	sh "$(TPM_DIR)/bin/install_plugins" || true
