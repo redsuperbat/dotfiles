2aea395f6804219f72144ccbfb77fcd8d1580acf
Max Netterberg <max.netterberg@gmail.com>
2025-01-20 13:26:11 +0100

    add vim.uv to global types

--- nvim/lua/max/plugins/nvim-lazydev.lua
+++ nvim/lua/max/plugins/nvim-lazydev.lua
@@ -5,6 +5,7 @@ return {
     library = {
       "luvit-meta/library",
       { path = "wezterm-types", mods = { "wezterm" } },
+      { path = "${3rd}/luv/library", words = { "vim%.uv" } },
     },
   },
 }
