68f12290e9fd9ac2a64c8039d6e0b4b29ecec04a
Max Netterberg <max.netterberg@gmail.com>
2025-02-02 20:21:59 +0100

    Fix fish.mk

--- fish/fish.mk
+++ fish/fish.mk
@@ -5,9 +5,9 @@ FISHER = fish/fisher/functions/fisher.fish
 fish: $(FISH) $(FISHER)
 
 $(FISH): $(BREW)
-  ln -his $(PWD)/fish $(CONFIG_DIR)/fish
-  $(BREW) install fish
+	ln -his $(PWD)/fish $(CONFIG_DIR)/fish
+	$(BREW) install fish
 
 $(FISHER): $(FISH)
-  fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher && fisher update'
+	fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher && fisher update'
 
