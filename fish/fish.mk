da8b7a54ca1d337eaf8fd3336f587a9d2ef398bc
Max Netterberg <max.netterberg@gmail.com>
2025-02-05 23:58:28 +0100

    Fix bindir fish

--- fish/fish.mk
+++ fish/fish.mk
@@ -12,5 +12,5 @@ $(FISH): $(BREW) $(CONFIG_DIR)
 
 
 $(FISHER): $(FISH)
-	$(BIN_DIR)fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher && fisher update'
+	$(BIN_DIR)/fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher && fisher update'
 
