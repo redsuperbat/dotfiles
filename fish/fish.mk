a5e912203024718184c2b31bb8918076b075bae4
Max Netterberg <max.netterberg@gmail.com>
2025-02-11 22:27:32 +0100

    use path instead of just name

--- fish/fish.mk
+++ fish/fish.mk
@@ -8,7 +8,7 @@ $(FISH): $(BREW) $(CONFIG_DIR)
 	ln -his $(PWD)/fish $(CONFIG_DIR)/fish
 	$(BREW) install fish
 	sudo sh -c 'echo "/opt/homebrew/bin/fish" >> /etc/shells'
-	sudo chsh -s fish $$(whoami)
+	sudo chsh -s /opt/homebrew/bin/fish $$(whoami)
 
 
 $(FISHER): $(FISH)
