69a550d88091c932938d6f1be072517743a58c86
Max Netterberg <max.netterberg@gmail.com>
2025-02-13 13:27:07 +0100

    Add block support in treesitter

--- nvim/lua/max/plugins/lsp/nvim-none-ls.lua
+++ nvim/lua/max/plugins/lsp/nvim-none-ls.lua
@@ -14,96 +14,100 @@ local lsp_formatting = function(bufnr)
 end
 
 return {
-  "nvimtools/none-ls.nvim",
-  event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-  dependencies = {
-    "davidmh/cspell.nvim",
-  },
-  keys = {
-    {
-      "<leader>cf",
-      function()
-        lsp_formatting(vim.api.nvim_get_current_buf())
-      end,
-      mode = { "n" },
-      desc = "Format buffer",
-    },
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    local cspell = require("cspell")
+  { "nvimtools/none-ls-extras.nvim", lazy = true },
+  { "davidmh/cspell.nvim", lazy = true },
 
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      callback = function()
-        lsp_formatting(vim.api.nvim_get_current_buf())
-      end,
-    })
-
-    local cspell_config = {
-      cspell_config_dirs = { "~/.config/nvim/lua/max/plugins/lsp/cspell/" },
-      on_add_to_json = function(payload)
-        local command = string.format(
-          "jq -S '.words |= sort' %s | sponge %s ",
-          payload.cspell_config_path,
-          payload.cspell_config_path
-        )
-        os.execute(command)
-      end,
-    }
-
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.code_actions.refactoring,
-
-        null_ls.builtins.diagnostics.actionlint,
-
-        cspell.diagnostics.with({
-          config = cspell_config,
-          diagnostics_postprocess = function(diagnostic)
-            diagnostic.severity = vim.diagnostic.severity.HINT
-          end,
-        }),
-        cspell.code_actions.with({ config = cspell_config }),
-
-        null_ls.builtins.formatting.sqlfluff.with({
-          extra_args = { "--dialect", "postgres" },
-        }),
-        null_ls.builtins.diagnostics.sqlfluff.with({
-          extra_args = { "--dialect", "postgres" },
-        }),
-
-        null_ls.builtins.formatting.shfmt,
-
-        null_ls.builtins.formatting.terraform_fmt,
-
-        null_ls.builtins.formatting.rubocop,
-        null_ls.builtins.diagnostics.rubocop,
-
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.biome.with({
-          args = {
-            "check",
-            "--write",
-            "--stdin-file-path",
-            "$FILENAME",
-          },
-        }),
-
-        null_ls.builtins.diagnostics.fish,
-        null_ls.builtins.formatting.fish_indent,
-
-        null_ls.builtins.formatting.prettier.with({
-          -- Exclude js/ts json because biome handles that
-          disabled_filetypes = {
-            "javascript",
-            "typescript",
-            "javascriptreact",
-            "typescriptreact",
-            "json",
-            "jsonc",
-          },
-        }),
+  {
+    "nvimtools/none-ls.nvim",
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    keys = {
+      {
+        "<leader>cf",
+        function()
+          lsp_formatting(vim.api.nvim_get_current_buf())
+        end,
+        mode = { "n" },
+        desc = "Format buffer",
       },
-    })
-  end,
+    },
+    config = function()
+      local null_ls = require("null-ls")
+      local cspell = require("cspell")
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        callback = function()
+          lsp_formatting(vim.api.nvim_get_current_buf())
+        end,
+      })
+
+      local cspell_config = {
+        cspell_config_dirs = { "~/.config/nvim/lua/max/plugins/lsp/cspell/" },
+        on_add_to_json = function(payload)
+          local command = string.format(
+            "jq -S '.words |= sort' %s | sponge %s ",
+            payload.cspell_config_path,
+            payload.cspell_config_path
+          )
+          os.execute(command)
+        end,
+      }
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.code_actions.refactoring,
+
+          null_ls.builtins.diagnostics.actionlint,
+
+          cspell.diagnostics.with({
+            config = cspell_config,
+            diagnostics_postprocess = function(diagnostic)
+              diagnostic.severity = vim.diagnostic.severity.HINT
+            end,
+          }),
+          cspell.code_actions.with({ config = cspell_config }),
+
+          null_ls.builtins.formatting.sqlfluff.with({
+            extra_args = { "--dialect", "postgres" },
+          }),
+          null_ls.builtins.diagnostics.sqlfluff.with({
+            extra_args = { "--dialect", "postgres" },
+          }),
+
+          null_ls.builtins.formatting.shfmt,
+
+          null_ls.builtins.formatting.terraform_fmt,
+
+          null_ls.builtins.formatting.rubocop,
+          null_ls.builtins.diagnostics.rubocop,
+
+          require("none-ls.diagnostics.eslint"),
+
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.biome.with({
+            args = {
+              "check",
+              "--write",
+              "--stdin-file-path",
+              "$FILENAME",
+            },
+          }),
+
+          null_ls.builtins.diagnostics.fish,
+          null_ls.builtins.formatting.fish_indent,
+
+          null_ls.builtins.formatting.prettier.with({
+            -- Exclude js/ts json because biome handles that
+            disabled_filetypes = {
+              "javascript",
+              "typescript",
+              "javascriptreact",
+              "typescriptreact",
+              "json",
+              "jsonc",
+            },
+          }),
+        },
+      })
+    end,
+  },
 }
--- nvim/lua/max/plugins/nvim-treesitter-textobjects.lua
+++ nvim/lua/max/plugins/nvim-treesitter-textobjects.lua
@@ -47,6 +47,9 @@ return {
             ["af"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
             ["if"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
 
+            ["ad"] = { query = "@block.outer", desc = "Select outer part of a block" },
+            ["id"] = { query = "@block.inner", desc = "Select inner part of a block" },
+
             ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
             ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
           },
