c44950141de7ac54e6600010a8c5313c8060a63d
Max Netterberg <max.netterberg@gmail.com>
2025-02-28 14:39:57 +0100

    Make everything lazy loaded

--- nvim/lua/max/lazy.lua
+++ nvim/lua/max/lazy.lua
@@ -1,17 +1,17 @@
 local fs = require("max.utils.fs")
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-if not fs.path_exists(lazypath) then
+if not fs.path_exists(lazy_path) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
-    lazypath,
+    lazy_path,
   })
 end
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.opt.rtp:prepend(vim.env.LAZY or lazy_path)
 
 require("lazy").setup({
   rocks = {
@@ -23,14 +23,13 @@ require("lazy").setup({
     { import = "max.plugins.lsp" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
+    lazy = true,
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
   },
   checker = { enabled = true, notify = false }, -- automatically check for plugin updates
+
   change_detection = {
     notify = false,
   },
