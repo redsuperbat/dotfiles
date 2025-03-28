64bfcae95b96ddb19bf75eea62cb42ece78681a6
Max Netterberg <max.netterberg@gmail.com>
2025-03-28 16:23:36 +0100

    Toggle text to lines

--- nvim/lua/max/config/diagnostics.lua
+++ nvim/lua/max/config/diagnostics.lua
@@ -3,6 +3,7 @@ vim.diagnostic.config({ virtual_text = true })
 vim.api.nvim_create_user_command("VirtualLinesToggle", function()
   vim.diagnostic.config({
     virtual_lines = not vim.diagnostic.config().virtual_lines,
+    virtual_text = not vim.diagnostic.config().virtual_text,
   })
 end, {
   desc = "Toggle virtual lines",
