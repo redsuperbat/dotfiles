81bd94cf021578e3ffff077a704ceb7e8e28cfea
Max Netterberg <max.netterberg@gmail.com>
2025-03-28 15:10:35 +0100

    Add leap.nvim

---
+++ nvim/lua/max/plugins/nvim-leap.lua
@@ -0,0 +1,16 @@
+return {
+  "ggandor/leap.nvim",
+  dependencies = { "tpope/vim-repeat" },
+  keys = {
+    {
+      "m",
+      "<Plug>(leap-forward)",
+      desc = "Leap forwards",
+    },
+    {
+      "M",
+      "<Plug>(leap-backward)",
+      desc = "Leap backwards",
+    },
+  },
+}
