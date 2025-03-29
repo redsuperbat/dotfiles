fea5e17b92a64cf02650dba068d5be5df4ae0d1f
Max Netterberg <max.netterberg@gmail.com>
2025-03-29 23:29:40 +0100

    Add lsp restart user command

--- nvim/lua/max/lsp.lua
+++ nvim/lua/max/lsp.lua
@@ -13,3 +13,12 @@ vim.lsp.enable({
 vim.api.nvim_create_user_command("LspInfo", ":checkhealth vim.lsp", {
   desc = "Language server info",
 })
+
+vim.api.nvim_create_user_command("LspRestart", function()
+  vim.lsp.stop_client(vim.lsp.get_clients())
+  vim.defer_fn(function()
+    vim.cmd("edit")
+  end, 1000)
+end, {
+  desc = "Restart all language servers",
+})
