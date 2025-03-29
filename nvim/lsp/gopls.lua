c3300695f687b895e9744b49b41564d48d847a4c
Max Netterberg <max.netterberg@gmail.com>
2025-03-30 00:17:12 +0100

    Add gopls

---
+++ nvim/lsp/gopls.lua
@@ -0,0 +1,10 @@
+---@type vim.lsp.Config
+return {
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_markers = {
+    "go.work",
+    "go.mod",
+    ".git",
+  },
+}
