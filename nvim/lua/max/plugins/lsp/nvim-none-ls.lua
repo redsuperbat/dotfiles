85a5b40347caf1f688ddfbdc715f5d35a04955fe
Max Netterberg <max.netterberg@gmail.com>
2025-03-29 22:14:46 +0100

    Remove refactoring lsp

--- nvim/lua/max/plugins/lsp/nvim-none-ls.lua
+++ nvim/lua/max/plugins/lsp/nvim-none-ls.lua
@@ -52,8 +52,6 @@ return {
 
     null_ls.setup({
       sources = {
-        null_ls.builtins.code_actions.refactoring,
-
         null_ls.builtins.diagnostics.actionlint,
 
         null_ls.builtins.formatting.sqlfluff.with({
