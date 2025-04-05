9ba4d6a9c90347dce35c4ff6e97adc717fb6387c
Max Netterberg <max.netterberg@gmail.com>
2025-04-05 13:19:38 +0200

    Add start lsp command

--- nvim/lua/max/lsp.lua
+++ nvim/lua/max/lsp.lua
@@ -15,14 +15,21 @@ vim.lsp.enable({
 })
 
 vim.api.nvim_create_user_command("LspLog", function()
-  local path = vim.fn.stdpath("state") .. "/lsp.log"
-  vim.cmd(string.format("view %s", path))
+  vim.cmd(string.format("view %s", vim.lsp.get_log_path()))
 end, {
   desc = "Open lsp logs",
 })
 
 vim.api.nvim_create_user_command("LspInfo", ":checkhealth vim.lsp", {
   desc = "Language server info",
+})
+
+vim.api.nvim_create_user_command("LspStart", function(ctx)
+  local config = vim.lsp.config[ctx.args]
+  vim.lsp.start(config)
+end, {
+  desc = "Open lsp logs",
+  nargs = 1,
 })
 
 vim.api.nvim_create_user_command("LspStop", function(ctx)
