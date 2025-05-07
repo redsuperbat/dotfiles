a59554ab62677f70a9edf4d5bc7c35867091fe97
Max Netterberg <max.netterberg@gmail.com>
2025-05-07 15:56:27 +0200

    remove unused commands

--- nvim/lua/max/plugins/none-ls.lua
+++ nvim/lua/max/plugins/none-ls.lua
@@ -1,18 +1,8 @@
 return {
   "nvimtools/none-ls.nvim",
   event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-  cmd = { "NullLsEnable", "NullLsDisable" },
   config = function()
     local null_ls = require("null-ls")
-
-    vim.api.nvim_create_user_command("NullLsEnable", function(ctx)
-      null_ls.enable({ name = ctx.args })
-    end, { desc = "Enable null ls source", nargs = 1 })
-
-    vim.api.nvim_create_user_command("NullLsDisable", function(ctx)
-      null_ls.disable({ name = ctx.args })
-    end, { desc = "Disable null ls source", nargs = 1 })
-
     null_ls.setup({
       sources = {
         -- Github actions
