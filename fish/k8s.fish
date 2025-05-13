3399a31a7dd64542c6643cc18a8e22a2682bfb64
Max Netterberg <max.netterberg@gmail.com>
2025-05-13 19:25:00 +0200

    update k8s scripts

--- fish/k8s.fish
+++ fish/k8s.fish
@@ -1,16 +1,16 @@
 function k8s_purge_pods
     echo 'Deleting all pods which do not have status "Running"...'
-    set NS $(kc get ns -o json | jq -r '.items[].metadata.name')
-    for ns in $NS
-        kc get pods -n $ns -o json \
+    set namespaces (kc get ns -o json | jq -r '.items[].metadata.name')
+    for namespace in $namespaces
+        kc get pods -n $namespace -o json \
             | jq -r '.items[] | select(.status.phase != "Running") | .metadata.name' \
-            | xargs kubectl delete pod -n $ns
+            | xargs kubectl delete pod -n $namespace
     end
     echo 'Pods deleted'
 end
 
 function k8s_temp_pod
     echo 'creating temp pod'
-    set RAND (random)
-    kubectl run tmp-debian-$RAND -i --rm --tty --image debian:bullseye --restart=Never -- bash
+    set random_number (random)
+    kubectl run tmp-debian-$random_number -i --rm --tty --image debian:bullseye --restart=Never -- bash
 end
