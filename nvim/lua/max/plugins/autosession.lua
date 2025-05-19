f9aa124e606c137492f13261d5c4b8a9500f1917
Max Netterberg <max.netterberg@gmail.com>
2025-05-19 22:03:59 +0200

    Do not save certain filetypes

--- nvim/lua/max/plugins/autosession.lua
+++ nvim/lua/max/plugins/autosession.lua
@@ -1,7 +1,13 @@
+-- Recommended session options for auto-session plugin
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
 return {
   "rmagatti/auto-session",
   lazy = false,
   ---@module "auto-session"
   ---@type AutoSession.Config
-  opts = { suppressed_dirs = { "~", "/" } },
+  opts = {
+    suppressed_dirs = { "~", "/" },
+    bypass_save_filetypes = { "neo-tree", "qf", "dashboard", "netrw" },
+  },
 }
