a77146770c7eafe3f10cf2b6b0546650d46f9346
Max Netterberg <max.netterberg@gmail.com>
2025-05-22 10:47:49 +0200

    Add better signs for diagnostics

--- nvim/lua/max/config/diagnostics.lua
+++ nvim/lua/max/config/diagnostics.lua
@@ -1,5 +1,10 @@
 vim.diagnostic.config({ virtual_text = true })
 
+for type, icon in pairs({ Error = " ", Warn = " ", Hint = " ", Info = " " }) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
 vim.api.nvim_create_user_command("VirtualLinesToggle", function()
   vim.diagnostic.config({
     virtual_lines = not vim.diagnostic.config().virtual_lines,
